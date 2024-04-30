57da39aaa0c29e88abc8e0f813c88eaee98f7603
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-04-30 00:49:11 -0300

    Update Procedure InsertPecaEmArmazem.sql

--- Procedure InsertPecaEmArmazem.sql
+++ Procedure InsertPecaEmArmazem.sql
@@ -1,10 +1,10 @@
-CREATE PROCEDURE InsertPecaEmArmazem
-    @CodigoPeça VARCHAR(50),
+CREATE PROCEDURE InserirPecaEmArmazem
+    @CodigoPeca VARCHAR(50),
     @Descricao VARCHAR(100),
     @CustoUnitario DECIMAL(10,2),
     @QuantidadeAtual INT
 AS
 BEGIN
-    INSERT INTO PeçaEmArmazem (CodigoPeça, Descricao, CustoUnitario, QuantidadeAtual)
-    VALUES (@CodigoPeça, @Descricao, @CustoUnitario, @QuantidadeAtual)
+    INSERT INTO PecaEmArmazem (CodigoPeca, Descricao, CustoUnitario, QuantidadeAtual)
+    VALUES (@CodigoPeca, @Descricao, @CustoUnitario, @QuantidadeAtual)
 END;

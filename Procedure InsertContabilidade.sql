8fdf3f7c58431af8395e2b18ba76f7d684acb27e
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-04-29 22:25:11 -0300

    Update Procedure InsertContabilidade.sql

--- Procedure InsertContabilidade.sql
+++ Procedure InsertContabilidade.sql
@@ -1,19 +1,18 @@
-CREATE PROCEDURE InserirTransacaoContabilidade
-    @DataTransacao DATE,
+CREATE PROCEDURE InsertContabilidade (
+    @IDPagamento DECIMAL(10,2),
+    @DataPagamento DATE,
     @Descricao VARCHAR(100),
-    @TipoTransacao VARCHAR(10),
-    @Valor MONEY,
-    @MetodoPagamento VARCHAR(20),
-    @NumeroFatura VARCHAR(50) = NULL,
-    @IDCliente INT = NULL,
-    @IDFornecedor INT = NULL,
-    @IDFuncionario INT = NULL,
-    @Categoria VARCHAR(50),
-    @Observacoes VARCHAR(MAX) = NULL
+    @TipoDePagamento VARCHAR(50),
+    @Valor DECIMAL(10,2),
+    @MetodoDePagamento VARCHAR(50),
+    @NumeroDaFatura VARCHAR(100),
+    @Fornecedor VARCHAR(100),
+    @Funcionario INT,
+    @Despesa VARCHAR(100),
+    @Observacoes VARCHAR(MAX)
+)
 AS
 BEGIN
-    SET NOCOUNT ON;
-
-    INSERT INTO Contabilidade (DataTransacao, Descricao, TipoTransacao, Valor, MetodoPagamento, NumeroFatura, IDCliente, IDFornecedor, IDFuncionario, Categoria, Observacoes)
-    VALUES (@DataTransacao, @Descricao, @TipoTransacao, @Valor, @MetodoPagamento, @NumeroFatura, @IDCliente, @IDFornecedor, @IDFuncionario, @Categoria, @Observacoes);
+    INSERT INTO Contabilidade (IDPagamento, DataPagamento, Descricao, TipoDePagamento, Valor, MetodoDePagamento, NumeroDaFatura, Fornecedor, Funcionario, Despesa, Observacoes)
+    VALUES (@IDPagamento, @DataPagamento, @Descricao, @TipoDePagamento, @Valor, @MetodoDePagamento, @NumeroDaFatura, @Fornecedor, @Funcionario, @Despesa, @Observacoes);
 END;

539fe34bf2979347c32c9f75de192ed15853bfe7
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-05-12 23:50:32 -0300

    Delete Procedure InsertTempoMaoDeObra.sql

--- Procedure InsertTempoMaoDeObra.sql
+++
@@ -1,9 +0,0 @@
-CREATE PROCEDURE InsertTempoMaoDeObra
-    @ReparacaoID INT,
-    @FuncionarioID INT,
-    @TempoGasto INT
-AS
-BEGIN
-    INSERT INTO TempoMaoDeObra (ReparacaoID, FuncionarioID, TempoGasto)
-    VALUES (@ReparacaoID, @FuncionarioID, @TempoGasto)
-END;

f29d044945d01dcd24806176e713af994564502d
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-05-12 23:45:38 -0300

    Delete Procedure InsertCustoHoraMaoDeObra.sql

--- Procedure InsertCustoHoraMaoDeObra.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE InsertCustoHoraMaoDeObra
-    @Categoria VARCHAR(50),
-    @CustoHora DECIMAL(10,2)
-AS
-BEGIN
-    INSERT INTO CustoHoraMaoDeObra (Categoria, CustoHora)
-    VALUES (@Categoria, @CustoHora)
-END;

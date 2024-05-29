cadb43ce5956d89e9aa5cbee570caa15c0e4592f
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-05-29 17:09:46 -0300

    Update Banco de Dados Oficina Mecânica.sql

--- Banco de Dados Oficina Mecânica.sql
+++ Banco de Dados Oficina Mecânica.sql
@@ -186,12 +186,11 @@ CREATE TABLE RelatorioEstatistica (
 );
 
 CREATE TABLE Contatos (
-    IDCliente INT,
+    IDCliente INT PRIMARY KEY,
     IDFuncionario INT,
     Telefone VARCHAR(20),
     Email VARCHAR(100),
     Morada VARCHAR(100),
-    PRIMARY KEY (IDCliente, IDFuncionario),
     FOREIGN KEY (IDCliente) REFERENCES Cliente(ClienteID),
     FOREIGN KEY (IDFuncionario) REFERENCES Funcionario(FuncionarioID)
 );

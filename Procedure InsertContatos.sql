476bfb8b64e7d38a70dbe09dfeee6008cc4e17b5
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-05-29 17:16:29 -0300

    Update Procedure InsertContatos.sql

--- Procedure InsertContatos.sql
+++ Procedure InsertContatos.sql
@@ -1,10 +1,20 @@
-CREATE TABLE Contatos (
-    IDCliente INT,
-    IDFuncionario INT,
-    Telefone VARCHAR(20),
-    Email VARCHAR(100),
-    Morada VARCHAR(100),
-    PRIMARY KEY (IDCliente, IDFuncionario),
-    FOREIGN KEY (IDCliente) REFERENCES Cliente(ClienteID),
-    FOREIGN KEY (IDFuncionario) REFERENCES Funcionario(FuncionarioID)
-);
+CREATE PROCEDURE InsertContato (
+    @IDCliente INT,
+    @IDFuncionario INT,
+    @Telefone VARCHAR(20),
+    @Email VARCHAR(100),
+    @Morada VARCHAR(100)
+)
+AS
+BEGIN
+    IF EXISTS (SELECT 1 FROM Cliente WHERE ClienteID = @IDCliente) AND EXISTS (SELECT 1 FROM Funcionario WHERE FuncionarioID = @IDFuncionario)
+    BEGIN
+        INSERT INTO Contatos (IDCliente, IDFuncionario, Telefone, Email, Morada)
+        VALUES (@IDCliente, @IDFuncionario, @Telefone, @Email, @Morada);
+        PRINT 'Contato inserido com sucesso.';
+    END
+    ELSE
+    BEGIN
+        PRINT 'Cliente ou funcionário não encontrado.';
+    END
+END;

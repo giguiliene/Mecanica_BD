85301276f63deffb9a8348f386ccf8d3ee36720d
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-06-04 00:58:22 -0300

    Update Procedure InsertCliente.sql

--- Procedure InsertCliente.sql
+++ Procedure InsertCliente.sql
@@ -2,7 +2,7 @@ CREATE PROCEDURE InserirCliente (
     @Nome VARCHAR(100),
     @DataNascimento DATE,
     @Genero VARCHAR(10),
-    @CPF VARCHAR(11),
+    @CPF VARCHAR(14),
     @EstadoCivil VARCHAR(20),
     @Observacao VARCHAR(MAX)
 )
@@ -12,4 +12,4 @@ BEGIN
     VALUES (@Nome, @DataNascimento, @Genero, @CPF, @EstadoCivil, @Observacao);
 END;
 
-EXEC InserirCliente 'Maria Silva', '1990-05-12', 'Feminino', '123.456.789-00', 'Solteiro(a)', 'Cliente regular.';
+EXEC InserirCliente 'Maria Silva', '1990-05-12', 'Feminino', '123.456.789.00', 'Solteiro(a)', 'Cliente regular.';
